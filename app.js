b7a45f1ddc029d2ae0ae5b3206e3d6320b00eee4
hansdav <hdavidson21@gmail.com>
2022-09-10 18:24:08 +0200

    functioning weather app, still needs more CSS done to look pretty

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-const div = document.querySelector("div")
+const searchbutton = document.querySelector("button#search").addEventListener("click", function() {
+    weather.search();
+});
+
+document.querySelector(".search-bar").addEventListener("keyup", function(e) {
+    if (e.key == "Enter") {
+        weather.search();
+    }
+});
 
 let weather = {
     apiKey: "3165b7995bfc5c9530915aa03d113f98",
@@ -10,6 +18,22 @@ let weather = {
             + this.apiKey
         )
             .then((response) => response.json())
-            .then((data) => console.log(data));
+            .then((data) => this.displayWeather(data));
+    },
+
+    displayWeather: function(data) {
+        const {name} = data;
+        const {icon, description} = data.weather[0];
+        const {temp, humidity} = data.main;
+        console.log(name,icon,description,temp,humidity);
+        document.querySelector(".city-name").innerText = name;
+        document.querySelector(".temperature").innerText = Math.round(temp - 273.15)+"°C";
+        document.querySelector("img.weather").src = "https://openweathermap.org/img/wn/"+icon+"@2x.png";
+        document.querySelector(".humidity").innerText = humidity+"%";
+
+    },
+
+    search: function() {
+        this.fetchWeather(document.querySelector(".search-bar").value)
     }
 };
